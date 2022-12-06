35b115b2b3d88a9a2797e1d46c856db77cff71d4
Hendrik Woelert <hendrik.woelert@online.de>
2022-12-06 11:01:53 +0100

    prevent encoding errors

--- sentence_filtering.py
+++ sentence_filtering.py
@@ -8,7 +8,7 @@ output_path = "./output/"
 
 sentences = []
 
-with open(input_file_path) as file:
+with open(input_file_path, encoding='utf-8') as file:
     sentences = [line.rstrip() for line in file]
     
 makedirs(output_path, exist_ok=True)
@@ -20,15 +20,17 @@ current_row = 0
 
 
 def approved():
-    #write new line  into  outputfile
     global sentence
-    with open(output_path+"empty_sentences.csv", 'a+') as f:
+    with open(output_path+"empty_sentences.csv", 'a+', encoding='utf-8') as f:
         f.write(sentence)
-        print(f"writing sentence {sentence}")
         f.write("\n")
     show_next_sentence()
 
 def disapproved():
+    global sentence
+    with open(output_path+"rejected_sentences.csv", 'a+', encoding='utf-8') as f:
+        f.write(sentence)
+        f.write("\n")
     show_next_sentence()
 
 def show_next_sentence():
@@ -36,7 +38,7 @@ def show_next_sentence():
     global sentence_label
     global current_row
     global row_label
-    #read input file and set sentence
+    
     current_row += 1
     sentence = sentences[current_row]
     sentence_label["text"]= sentence
@@ -47,7 +49,6 @@ def show_next_sentence():
 def save():
     with open(input_file_path, 'w', encoding='utf-8') as fp: 
         for item in sentences[current_row:]: 
-        # write each item on a new line 
             try: 
                 fp.write(f"{item}\n")
             except:
@@ -64,10 +65,8 @@ ttk.Button(root, text="JA", command=approved).pack()
 ttk.Button(root, text="NEIN", command=disapproved).pack()
 ttk.Label(root, text="Die Kategorien sind: Mobilität, CO2-Entfernung, Energieeffizienz, Circular, \nErneuerbare, CO2-Einsparung, Prozessemissionen, Kompensation, Klimarisikpanalyse, Klimabilanz, Klimaresilienz, Lebensmittel, Transport").pack()
 row_label=ttk.Label(root, text=f"Satz {current_row} von {num_of_sentences}")
-# row_label.grid(column=0, row=3, sticky="news")
 row_label.pack()
 sentence_label = Message(root, text=sentence, font=('Arial', 25), width=750)
-# sentence_label.grid(row=0, columnspan=2, sticky="news")
 sentence_label.pack()
 ttk.Button(root, text="save", command=save).pack()
 
